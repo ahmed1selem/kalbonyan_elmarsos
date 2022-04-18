5d7413aa7e238a556bfaf2f0a3d0b076ac35b778
ahmed1selem <95752195+ahmed1selem@users.noreply.github.com>
2022-04-18 18:28:55 +0200

    Update collection.py

--- linkedin courses/beyond fundamentals/collection.py
+++ linkedin courses/beyond fundamentals/collection.py
@@ -10,6 +10,5 @@ Highest_peak_each_tectonic_plate ={"African" : 'Kilimanjaro'
 ,"North_American" : 'Denali'
 ,"Pacific" : 'Mauna Kea'
 ,"South_American" :'Aconcagua'}
-for i in Nearest_stars_to_Earth:
-    print(i)
+print(Nearest_stars_to_Earth(3))
 print(Highest_peak_each_ِtectonic_plate["African"])
