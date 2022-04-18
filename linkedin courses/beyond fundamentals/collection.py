efd6c045b7a43f0e5018f4d2944776a8669637be
ahmed1selem <95752195+ahmed1selem@users.noreply.github.com>
2022-04-18 18:23:56 +0200

    Create collection.py

---
+++ linkedin courses/beyond fundamentals/collection.py
@@ -0,0 +1,15 @@
+Nearest_stars_to_Earth=[ 'Sol'
+ ,'Alpha Centauri'
+ ,'Barnard'
+,'Wolf 359',]
+
+Highest_peak_each_tectonic_plate ={"African" : 'Kilimanjaro'
+,"Antarctic" :'Vinson'
+,"Australian" : 'Puncak Jaya'
+,"Eurasian" : 'Everest'
+,"North_American" : 'Denali'
+,"Pacific" : 'Mauna Kea'
+,"South_American" :'Aconcagua'}
+for i in Nearest_stars_to_Earth:
+    print(i)
+print(Highest_peak_each_ِtectonic_plate["African"])
