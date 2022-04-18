b1ddc74fe89d8ecf7223b037fd6e640b9f6d6a58
ahmed1selem <95752195+ahmed1selem@users.noreply.github.com>
2022-04-18 17:52:20 +0200

    Create Challenge2_guessing _game.py

---
+++ linkedin courses/Programming Foundations/Challenge2_guessing _game.py
@@ -0,0 +1,6 @@
+fav_pick=input(print("what is your favorite food"))
+if fav_pick=="koshari":
+               print("Yep! So amazing!")
+ else:
+               print("Yuk! That's not it ")
+print("thanks for playing")               
