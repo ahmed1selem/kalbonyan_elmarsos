2ea4d02ec807ff836beb5e52ff93c6a15d2d39f4
ahmed1selem <95752195+ahmed1selem@users.noreply.github.com>
2022-08-17 03:17:46 +0200

    Delete linkedin courses directory

--- linkedin courses/Programming Foundations/guessing _game.py
+++
@@ -1,6 +0,0 @@
-fav_pick=input(print("what is your favorite food"))
-if fav_pick=="koshari":
-               print("Yep! So amazing!")
- else:
-               print("Yuk! That's not it ")
-print("thanks for playing")               
